1eb7f65dc2f5745246a6a8e6e044b60e96cbb742
FLASH-EXE <166912770+FLASH-EXE@users.noreply.github.com>
2024-07-18 16:15:51 +0545

    Update flash.py

--- flash.py
+++ flash.py
@@ -8,13 +8,13 @@ print(' \u001b[37m\x1b[38;5;196m>> \033[1;97mChecking For Update...')
 os.system('git pull --quiet ')
 bit = platform.architecture()[0]
 if bit == "64bit":
- print( '\u001b[37m\x1b[38;5;196m>>\033[1;97m64Bit Found')
+ print(' \u001b[37m\x1b[38;5;196m>>\033[1;97m64Bit Found')
  from flash import login
  login()
  
  
  
 if bit == "32bit":
- print( '\u001b[37m\x1b[38;5;196m>> \033[1;97m32Bit Found')
+ print(' \u001b[37m\x1b[38;5;196m>> \033[1;97m32Bit Found')
  from flash import login
  login()
